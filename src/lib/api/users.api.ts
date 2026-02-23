83b1cf499fd693bfa177115e68fc5c12f17cc768
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-23 22:50:04 +0000

    fetchUsers apiCall

--- src/lib/api/users.api.ts
+++ src/lib/api/users.api.ts
@@ -2,10 +2,21 @@ import { Role } from "@/types/role";
 import { ServerResponse } from "@/types/server";
 import apiClient from "../api";
 import toast from "react-hot-toast";
+import { UserType } from "@/types/User";
 
 export async function fetchRoles(): Promise<Role[]> {
   try {
     const { data }: ServerResponse<Role[]> = await apiClient.get("/roles");
+    return data;
+  } catch (error) {
+    toast.error("An error has occurred.");
+    return [];
+  }
+}
+
+export async function fetchUsers(): Promise<UserType[]> {
+  try {
+    const {data}: ServerResponse<UserType[]> = await apiClient.get("/users");
     return data;
   } catch (error) {
     toast.error("An error has occurred.");
