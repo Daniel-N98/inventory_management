c877dcf71de3917d3f0eaec8b1442cb04b9ea6fa
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-23 19:11:52 +0000

    Axios interceptor setup

---
+++ src/lib/api.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+const apiClient = axios.create({
+  baseURL: "/api",
+});
+
+apiClient.interceptors.response.use(
+  function (response) {
+    return response.data;
+  },
+  function (error) {
+    console.error(error.message);
+
+    if (error.message) {
+      toast.error(error.message);
+    } else {
+      toast.error("something went wrong...");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
