99b8131c87906d62aa560bd23ef0d82b4df3db85
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-23 22:58:15 +0000

    Mapping role._id to name

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,7 @@
 import dbConnect from "@/lib/mongodb"
+import Roles from "@/models/Roles";
 import User from "@/models/User";
+import { Role } from "@/types/role";
 import { UserType } from "@/types/User"
 import { NextResponse } from "next/server"
 
@@ -8,12 +10,13 @@ export async function GET() {
 
   try {
     const users = await User.find({}).lean();
-
+    const roles = await Roles.find({}).lean();
+    
     const formatted: UserType[] = users.map((user: UserType) => ({
       _id: user._id.toString(),
       name: user.name,
       email: user.email,
-      role: user.role,
+      role: roles.find((role: Role) => role._id.toString() === user.role.toString()).name, // Map role._id to role name
     }));
 
     return NextResponse.json(
