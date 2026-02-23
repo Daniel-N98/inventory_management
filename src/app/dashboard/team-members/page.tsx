3c7f3915103e69db7e231c77da1d62ba2cb6f9a3
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-23 23:01:30 +0000

    Extracted TeamMember table

--- src/app/dashboard/team-members/page.tsx
+++ src/app/dashboard/team-members/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import TeamMemberTable from "@/components/ui/team-members/teamMemberTable";
 import { fetchUsers } from "@/lib/api/users.api";
-import { UserType } from "@/types/User";
-import { Pencil } from "lucide-react";
+import { UserType } from "@/types/user";
 import { useEffect, useState } from "react";
 
 export default function TeamMembers() {
@@ -42,45 +40,7 @@ export default function TeamMembers() {
           className="bg-white dark:bg-zinc-700 text-zinc-900 dark:text-zinc-50"
         />
       </div>
-      <Card className="bg-white dark:bg-zinc-800 w-full overflow-x-auto" >
-        {/* Inventory Table */}
-        <CardHeader>
-          <CardTitle className="text-zinc-900 dark:text-zinc-50">Inventory Items</CardTitle>
-        </CardHeader>
-        <CardContent className="p-0">
-          <div className="overflow-x-auto">
-            <Table className="min-w-175">
-              <TableHeader>
-                <TableRow className="bg-zinc-100 dark:bg-zinc-700">
-                  <TableHead className="w-12">ID</TableHead>
-                  <TableHead className="w-36">Name</TableHead>
-                  <TableHead className="w-60">Email</TableHead>
-                  <TableHead className="w-48">Role</TableHead>
-                  <TableHead className="w-16 text-center">Actions</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {filtered &&
-                  filtered.map((item: UserType, idx: number) => (
-                    <TableRow
-                      key={item._id}
-                      className={`${idx % 2 === 0 ? "bg-zinc-50 dark:bg-zinc-900" : "bg-white dark:bg-zinc-800"
-                        } hover:bg-zinc-200 dark:hover:bg-zinc-700`}
-                    >
-                      <TableCell>{idx + 1}</TableCell>
-                      <TableCell>{item.name}</TableCell>
-                      <TableCell>{item.email}</TableCell>
-                      <TableCell>{item.role}</TableCell>
-                      <TableCell className="flex justify-center">
-                        <Pencil className="h-5 w-5 text-blue-600 hover:text-blue-700 cursor-pointer" />
-                      </TableCell>
-                    </TableRow>
-                  ))}
-              </TableBody>
-            </Table>
-          </div>
-        </CardContent>
-      </Card>
+      <TeamMemberTable filtered={filtered} />
     </main>
   )
 }
