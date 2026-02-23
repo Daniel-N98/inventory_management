7b1f46b81a2b747584dab3612eaad35101e2df29
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-23 18:23:40 +0000

    Created a CategoriesDropdown

---
+++ src/components/ui/category/categoriesDropdown.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { 
+  DropdownMenu, 
+  DropdownMenuTrigger, 
+  DropdownMenuContent 
+} from "@/components/ui/dropdown-menu";
+import { Command, CommandInput, CommandEmpty, CommandGroup, CommandItem } from "@/components/ui/command";
+import { CategoriesType } from "@/types/category";
+
+interface CategoriesDropdownProps {
+  categories: CategoriesType[];
+  selected?: null;
+  onSelect(category: CategoriesType): void;
+}
+
+export default function CategoriesDropdown({ categories, onSelect }: CategoriesDropdownProps) {
+  const [open, setOpen] = useState(false);
+  const [selectedName, setSelectedName] = useState<string>("Select a category");
+  
+  return (
+    <DropdownMenu open={open} onOpenChange={setOpen} >
+      <DropdownMenuTrigger asChild className="bg-white text-black hover:bg-white hover:border-zinc-950 hover:border">
+        <Button className="w-full justify-between">
+          {selectedName}
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="w-full p-0">
+        <Command>
+          <CommandInput placeholder="Search categories..." />
+          <CommandEmpty>No categories found.</CommandEmpty>
+          <CommandGroup>
+            {categories.map(cat => (
+              <CommandItem
+                key={cat._id}
+                onSelect={() => {
+                  setSelectedName(cat.name); // update button text
+                  onSelect(cat);            // update hidden input in parent
+                  setOpen(false);
+                }}
+              >
+                {cat.name}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
