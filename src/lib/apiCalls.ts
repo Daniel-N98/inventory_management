55210add94bba83232357a3a7e97e02b1d237498
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-23 19:51:58 +0000

    InventoryItems API Call

--- src/lib/apiCalls.ts
+++ src/lib/apiCalls.ts
@@ -1,3 +1,4 @@
+import { InventoryItem } from "@/types/inventory";
 import apiClient from "./api";
 import { CategoriesType } from "@/types/category";
 
@@ -12,6 +13,16 @@ export async function fetchCategories(): Promise<CategoriesType[]> {
     return data;
   } catch (err) {
     console.error("Error fetching categories:", err);
+    return [];
+  }
+}
+
+export async function fetchInventoryItems(): Promise<InventoryItem[]> {
+  try {
+    const { data }: ServerResponse<InventoryItem[]> = await apiClient.get("/inventory-item");
+    return data;
+  } catch (err) {
+    console.error("Error fetching inventory items:", err);
     return [];
   }
 }
