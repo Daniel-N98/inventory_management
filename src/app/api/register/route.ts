9a2c5c0cd33ec7368e1f88416b43f81eba739bea
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-24 03:09:01 +0000

    Updated register

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,16 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import { MongoClient } from "mongodb";
 import bcrypt from "bcrypt";
-
-let client: MongoClient | null = null;
-
-async function connectToDatabase() {
-  if (!client) {
-    client = new MongoClient(process.env.MONGODB_URI!);
-    await client.connect();
-  }
-  return client.db("inventory_management");
-}
+import User from "@/models/User";
+import dbConnect from "@/lib/mongodb";
 
 export async function POST(req: NextRequest) {
   const { name, email, password, role = "699cd39cb37779ad96ab9654" } = await req.json();
@@ -19,18 +10,17 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ error: "Missing fields" }, { status: 400 });
   }
 
-  const db = await connectToDatabase();
+  await dbConnect();
 
   // check if user exists
-  const existing = await db.collection("users").findOne({ email });
+  const existing = await User.findOne({ email });
   if (existing) {
     return NextResponse.json({ error: "User already exists" }, { status: 400 });
   }
 
   // hash password with bcrypt
   const hashedPassword = await bcrypt.hash(password, 12);
-
-  const result = await db.collection("users").insertOne({
+  const result = await User.create({
     name,
     email,
     password: hashedPassword,
