7ae16d68cd50b09f4b1cff841cdc016835c52abf
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-24 16:34:36 +0000

    Removed toast from api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { toast } from "react-hot-toast";
 
 const apiClient = axios.create({
   baseURL: "/api",
@@ -11,12 +10,6 @@ apiClient.interceptors.response.use(
   },
   function (error) {
     console.error(error.message);
-
-    if (error.message) {
-      toast.error(error.message);
-    } else {
-      toast.error("something went wrong...");
-    }
     return Promise.reject(error);
   }
 );
