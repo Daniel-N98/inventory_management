c200f684c7c8986fe19e57f680734b5812a8715d
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-24 03:25:38 +0000

    Moved setting and sign out to bottom of nav bar

--- src/components/sidebar-nav.tsx
+++ src/components/sidebar-nav.tsx
@@ -10,27 +10,40 @@ export default function SideNav() {
     { label: "Inventory", icon: <Package className="h-4 w-4" />, href: "/dashboard/inventory" },
     { label: "Categories", icon: <ListChecks className="h-4 w-4" />, href: "/dashboard/categories" },
     { label: "Team Members", icon: <Users className="h-4 w-4" />, href: "/dashboard/team-members" },
-    { label: "Settings", icon: <Settings className="h-4 w-4" />, href: "/dashboard/settings" },
   ];
 
   return (
     <aside className="w-full md:w-64 border-b md:border-b-0 md:border-r border-zinc-200 dark:border-zinc-700 bg-white dark:bg-zinc-800 p-6 flex flex-col">
-      <h2 className="text-2xl font-bold mb-8 text-zinc-900 dark:text-zinc-50">Inventory</h2>
-      <nav className="flex flex-row md:flex-col gap-2 md:gap-3 overflow-x-auto md:overflow-visible">
-        {navItems.map((item) => (
+      <h2 className="text-2xl font-bold mb-8 text-zinc-900 dark:text-zinc-50">InvManager</h2>
+      <nav className="flex flex-col justify-between h-full">
+        <div className="flex flex-row md:flex-col gap-2 md:gap-3 overflow-x-auto md:overflow-visible">
+          {navItems.map((item) => (
+            <Button
+              key={item.label}
+              variant="ghost"
+              className="group flex items-center gap-2 justify-center md:justify-start text-zinc-800 dark:text-zinc-50 hover:bg-zinc-100 dark:hover:bg-zinc-700 w-full"
+              asChild
+            >
+              <a href={item.href} className="flex items-center gap-2 w-full">
+                {item.icon}
+                <span className="hidden md:inline">{item.label}</span>
+              </a>
+            </Button>
+          ))}
+        </div>
+        <div className="space-y-4">
           <Button
-            key={item.label}
             variant="ghost"
             className="group flex items-center gap-2 justify-center md:justify-start text-zinc-800 dark:text-zinc-50 hover:bg-zinc-100 dark:hover:bg-zinc-700 w-full"
             asChild
           >
-            <a href={item.href} className="flex items-center gap-2 w-full">
-              {item.icon}
-              <span className="hidden md:inline">{item.label}</span>
+            <a href={"dashboard/settings"} className="flex items-center gap-2 w-full">
+              <Settings className="h-4 w-4" />
+              <span className="hidden md:inline">Settings</span>
             </a>
           </Button>
-        ))}
-        <Button onClick={() => signOut()}>Sign out</Button>
+          <Button onClick={() => signOut()} className="w-full">Sign out</Button>
+        </div>
       </nav>
     </aside>
   );
