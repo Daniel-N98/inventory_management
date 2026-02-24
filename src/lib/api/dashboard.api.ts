165bc014e831db50d89d203dd3d4977aa05860f5
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-24 16:32:18 +0000

    Potential abort error fix.

--- src/lib/api/dashboard.api.ts
+++ src/lib/api/dashboard.api.ts
@@ -1,18 +1,15 @@
 import { DashboardData } from "@/types/dashboard";
 import apiClient from "../api";
-import { CategoriesType } from "@/types/category";
-import { ServerResponse } from "@/types/server";
-import { InventoryItem } from "@/types/inventory";
-import { UserType } from "@/types/user";
-import { Role } from "@/types/role";
 import toast from "react-hot-toast";
 
 export async function fetchDashboardData(): Promise<DashboardData | null> {
   try {
-    const categoriesRes: ServerResponse<CategoriesType[]> = await apiClient.get("/categories");
-    const inventoryItemsRes: ServerResponse<InventoryItem[]> = await apiClient.get("/inventory-item");
-    const usersRes: ServerResponse<UserType[]> = await apiClient.get("/users");
-    const rolesRes: ServerResponse<Role[]> = await apiClient.get("/roles");
+    const [categoriesRes, inventoryItemsRes, usersRes, rolesRes] = await Promise.all([
+      await apiClient.get("/categories"),
+      await apiClient.get("/inventory-item"),
+      await apiClient.get("/users"),
+      await apiClient.get("/roles"),
+    ]);
 
     const dashboardData: DashboardData = {
       categories: categoriesRes.data,
