ec7adde1dbbb00916f6664e97c769bed93f8ad21
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-26 21:39:29 +0000

    Route and Api call for fetching site settings

--- src/app/api/site-settings/route.ts
+++ src/app/api/site-settings/route.ts
@@ -2,7 +2,45 @@ import dbConnect from "@/lib/mongodb";
 import { requireAuth } from "@/lib/requireAuth";
 import Roles from "@/models/Roles";
 import SiteSettings from "@/models/SiteSettings";
+import { FormattedSiteSettings } from "@/types/site-settings";
 import { NextResponse } from "next/server";
+
+const SITE_SETTINGS_NAME_MAPPINGS: any = {
+  "site-settings": "Site Settings",
+  "inventory-items": "Inventory Items",
+  "categories": "Categories",
+  "roles": "Roles",
+  "team-members": "Team Members"
+}
+
+export async function GET() {
+  await dbConnect()
+
+  try {
+    const siteSettings = await SiteSettings.find({}).lean();
+    const roles = await Roles.find({}).lean();
+    
+    const formatted: FormattedSiteSettings[] = siteSettings.reduce((acc, siteSetting) => {
+      const name = SITE_SETTINGS_NAME_MAPPINGS[siteSetting.name];
+      acc[name] = {
+        editRole: roles.find(r => r._id.toString() === siteSetting.editRole?.toString())?.name || null,
+        createRole: roles.find(r => r._id.toString() === siteSetting.createRole?.toString())?.name || null,
+        inviteRole: roles.find(r => r._id.toString() === siteSetting.inviteRole?.toString())?.name || null,
+      };
+      return acc;
+    }, {} as Record<string, { editRole: string | null, createRole: string | null, inviteRole: string | null }>);
+    
+    return NextResponse.json(
+      { success: true, data: formatted },
+      { status: 200 }
+    )
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error },
+      { status: 500 }
+    )
+  }
+}
 
 export async function PATCH(request: Request) {
   await dbConnect()
--- src/lib/api/site-settings.api.ts
+++ src/lib/api/site-settings.api.ts
@@ -1,10 +1,20 @@
 import apiClient from "../api";
 import toast from "react-hot-toast";
-import { ServerResponseType } from "@/types/site-settings";
+import { FormattedSiteSettings, ServerResponseType, SiteSettingsType } from "@/types/site-settings";
+
+export async function fetchSiteSettings(): Promise<FormattedSiteSettings | null> {
+  try {
+    const { data } = await apiClient.get("/site-settings");
+    return data;
+  } catch (error) {
+    console.log("An error has occurred.");
+    return null;
+  }
+}
 
 export async function updateSiteSettings(type: string, editRole: string, createRole: string, inviteRole?: string): Promise<ServerResponseType> {
   try {
-    const { data, error }: {data: ServerResponseType, error: string} = await apiClient.patch("/site-settings", { type, editRole, createRole, inviteRole });
+    const { data, error }: { data: ServerResponseType, error: string } = await apiClient.patch("/site-settings", { type, editRole, createRole, inviteRole });
     if (error) {
       toast.error(error);
       return { editSuccess: false, createSuccess: false, inviteSuccess: false, };
