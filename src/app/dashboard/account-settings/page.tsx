67dffca48846245151912aa51c636cd55b6ae836
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-27 00:11:13 +0000

    Super user toggle in account settings

--- src/app/dashboard/account-settings/page.tsx
+++ src/app/dashboard/account-settings/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
+import { Field, FieldContent, FieldDescription, FieldGroup, FieldLabel, FieldTitle } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import { updateUserName, updateUserPassword } from "@/lib/api/users.api";
+import LoadingIcon from "@/components/ui/loadingIcon";
+import { Switch } from "@/components/ui/switch";
+import { checkSuperUser, toggleSuperUser, updateUserName, updateUserPassword } from "@/lib/api/users.api";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -15,6 +17,7 @@ export default function Settings() {
 
   const [profile, setProfile] = useState({ name: "John Doe", email: "john@example.com" });
   const [loading, setLoading] = useState<boolean>(true);
+  const [isSuperUser, setIsSuperUser] = useState<boolean>(false);
 
   useEffect(() => {
     if (status === "loading") return; // wait until session is loaded
@@ -30,11 +33,25 @@ export default function Settings() {
             name: session.user.name ?? "",
             email: session.user.email ?? "",
           });
-          setLoading(false);
         }
       }
     }
+
+    async function loadisSuperUser() {
+
+      const isSuperUserRes: boolean = await checkSuperUser();
+      setIsSuperUser(isSuperUserRes);
+      setLoading(false);
+    }
+    loadisSuperUser();
   }, [status, router, session]);
+
+  async function handleToggleSuperUser() {
+    const result: boolean = await toggleSuperUser(!isSuperUser);
+    if (result) {
+      setIsSuperUser(!isSuperUser);
+    }
+  }
 
 
   function handleProfileChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -90,91 +107,110 @@ export default function Settings() {
       <div className="mb-6 md:mb-8 gap-4">
         <h1 className="text-2xl md:text-3xl font-semibold text-zinc-900 dark:text-zinc-50">Account Settings</h1>
       </div>
-      <div className="grid grid-cols-1 gap-6 lg:grid-cols-2 xl:grid-cols-3">
-        {/* Profile Settings */}
-        <Card className="mb-6 bg-white dark:bg-zinc-800">
-          <CardHeader>
-            <CardTitle className="text-zinc-900 dark:text-zinc-50">Profile</CardTitle>
-          </CardHeader>
-          <form onSubmit={updateProfile} className="space-y-4">
-            <CardContent className="space-y-4">
-              <div className="flex flex-col md:flex-row gap-4">
-                <Field>
-                  <FieldLabel htmlFor="name">Name</FieldLabel>
-                  <FieldDescription>Your profile name.</FieldDescription>
-                  <Input
-                    id="name"
-                    name="name"
-                    value={profile.name}
-                    onChange={handleProfileChange}
-                    className="flex-1 bg-white dark:bg-zinc-700 text-zinc-900 dark:text-zinc-50"
-                    disabled={loading}
-                  />
-                </Field>
-                <Field>
-                  <FieldLabel htmlFor="email">Email</FieldLabel>
-                  <FieldDescription>Your profile email.</FieldDescription>
-                  <Input
-                    id="email"
-                    name="email"
-                    value={profile.email}
-                    onChange={handleProfileChange}
-                    className="flex-1 bg-white dark:bg-zinc-700 text-zinc-900 dark:text-zinc-50"
-                    disabled
-                  />
-                </Field>
-              </div>
-              <Button className="bg-blue-600 hover:bg-blue-700 text-white" disabled={loading}>Update Profile</Button>
-            </CardContent>
-          </form>
-        </Card>
+      {!loading ?
+        <>
+          {/* SuperUser toggle */}
+          <FieldGroup className="w-full max-w-sm mb-4" onClick={() => handleToggleSuperUser()}>
+            <FieldLabel htmlFor="switch-share">
+              <Field orientation="horizontal">
+                <FieldContent>
+                  <FieldTitle>Toggle Super User</FieldTitle>
+                  <FieldDescription>
+                    For testing purposes, you can toggle Super User account here. This gives you full access to all aspects of the website.
+                  </FieldDescription>
+                </FieldContent>
+                <Switch id="switch-share" defaultChecked={isSuperUser} />
+              </Field>
+            </FieldLabel>
+          </FieldGroup>
 
-        {/* Password Settings */}
-        <Card className="mb-6 bg-white dark:bg-zinc-800">
-          <CardHeader>
-            <CardTitle className="text-zinc-900 dark:text-zinc-50">Password</CardTitle>
-          </CardHeader>
-          <form onSubmit={updatePassword} className="space-y-4">
+          <div className="grid grid-cols-1 gap-6 lg:grid-cols-2 xl:grid-cols-3">
+            {/* Profile Settings */}
+            <Card className="mb-6 bg-white dark:bg-zinc-800">
+              <CardHeader>
+                <CardTitle className="text-zinc-900 dark:text-zinc-50">Profile</CardTitle>
+              </CardHeader>
+              <form onSubmit={updateProfile} className="space-y-4">
+                <CardContent className="space-y-4">
+                  <div className="flex flex-col gap-4">
+                    <Field>
+                      <FieldLabel htmlFor="name">Name</FieldLabel>
+                      <FieldDescription>Your profile name.</FieldDescription>
+                      <Input
+                        id="name"
+                        name="name"
+                        value={profile.name}
+                        onChange={handleProfileChange}
+                        className="flex-1 bg-white dark:bg-zinc-700 text-zinc-900 dark:text-zinc-50"
+                        disabled={loading}
+                      />
+                    </Field>
+                    <Field>
+                      <FieldLabel htmlFor="email">Email</FieldLabel>
+                      <FieldDescription>Your profile email.</FieldDescription>
+                      <Input
+                        id="email"
+                        name="email"
+                        value={profile.email}
+                        onChange={handleProfileChange}
+                        className="flex-1 bg-white dark:bg-zinc-700 text-zinc-900 dark:text-zinc-50"
+                        disabled
+                      />
+                    </Field>
+                  </div>
+                  <Button className="bg-blue-600 hover:bg-blue-700 text-white" disabled={loading}>Update Profile</Button>
+                </CardContent>
+              </form>
+            </Card>
 
-            <CardContent className="space-y-4">
-              <Field>
-                <FieldLabel htmlFor="current-password">Current password</FieldLabel>
-                <FieldDescription>Your current profile password.</FieldDescription>
-                <Input
-                  id="current-password"
-                  type="password"
-                  name="current-password"
-                  className="bg-white dark:bg-zinc-700 text-zinc-900 dark:text-zinc-50"
-                  disabled={loading}
-                />
-              </Field>
-              <Field>
-                <FieldLabel htmlFor="new-password">New password</FieldLabel>
-                <FieldDescription>Your new profile password.</FieldDescription>
-                <Input
-                  id="new-password"
-                  type="password"
-                  name="new-password"
-                  className="bg-white dark:bg-zinc-700 text-zinc-900 dark:text-zinc-50"
-                  disabled={loading}
-                />
-              </Field>
-              <Field>
-                <FieldLabel htmlFor="new-password-confirm"></FieldLabel>
-                <FieldDescription>Confirm your new profile password.</FieldDescription>
-                <Input
-                  id="new-password-confirm"
-                  type="password"
-                  name="new-password-confirm"
-                  className="bg-white dark:bg-zinc-700 text-zinc-900 dark:text-zinc-50"
-                  disabled={loading}
-                />
-              </Field>
-              <Button className="bg-blue-600 hover:bg-blue-700 text-white" disabled={loading}>Update Password</Button>
-            </CardContent>
-          </form>
-        </Card>
-      </div>
-    </div>
+            {/* Password Settings */}
+            <Card className="mb-6 bg-white dark:bg-zinc-800">
+              <CardHeader>
+                <CardTitle className="text-zinc-900 dark:text-zinc-50">Password</CardTitle>
+              </CardHeader>
+              <form onSubmit={updatePassword} className="space-y-4">
+
+                <CardContent className="space-y-4">
+                  <Field>
+                    <FieldLabel htmlFor="current-password">Current password</FieldLabel>
+                    <FieldDescription>Your current profile password.</FieldDescription>
+                    <Input
+                      id="current-password"
+                      type="password"
+                      name="current-password"
+                      className="bg-white dark:bg-zinc-700 text-zinc-900 dark:text-zinc-50"
+                      disabled={loading}
+                    />
+                  </Field>
+                  <Field>
+                    <FieldLabel htmlFor="new-password">New password</FieldLabel>
+                    <FieldDescription>Your new profile password.</FieldDescription>
+                    <Input
+                      id="new-password"
+                      type="password"
+                      name="new-password"
+                      className="bg-white dark:bg-zinc-700 text-zinc-900 dark:text-zinc-50"
+                      disabled={loading}
+                    />
+                  </Field>
+                  <Field>
+                    <FieldLabel htmlFor="new-password-confirm"></FieldLabel>
+                    <FieldDescription>Confirm your new profile password.</FieldDescription>
+                    <Input
+                      id="new-password-confirm"
+                      type="password"
+                      name="new-password-confirm"
+                      className="bg-white dark:bg-zinc-700 text-zinc-900 dark:text-zinc-50"
+                      disabled={loading}
+                    />
+                  </Field>
+                  <Button className="bg-blue-600 hover:bg-blue-700 text-white" disabled={loading}>Update Password</Button>
+                </CardContent>
+              </form>
+            </Card>
+          </div>
+        </> : <LoadingIcon />
+      }
+    </div >
   );
 }
